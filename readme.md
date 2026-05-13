dfb4cf5fba40286c3e57915cefae03e0cfb92ca3
Lauri <lauri.etelamaki@gmail.com>
2026-05-13 21:37:23 +0300

    Plotting across frequency range

--- readme.md
+++ readme.md
@@ -1,5 +1,8 @@
-# Simple program that produces visual RSSI spectrum of selected freq. range.  
+# Simple program for RTL-SDR that produces visual RSSI spectrum of selected freq. range. 
 
+Uses pyplot and pyrtlsdr
+
+![image](./dev/plot_1.png)
 
 ### libaries
 pyrtlsdr library: https://pypi.org/project/pyrtlsdr/ 
---
+++ src/main.py
@@ -0,0 +1,78 @@
+# import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.pyplot as plt
+from rtlsdr import *
+from time import gmtime, strftime
+
+sdr = RtlSdr()
+
+def configRTL(bw, gain):
+    # config
+    sdr.sample_rate = bw * 1e6 # 2.4 MHz bandwidth
+    sdr.gain = gain
+
+block_freqs = []
+block_psd_charts = []
+
+def remove_block_dc_spike(p, f, width=10):
+    cleaned = p.copy()
+
+    center = len(cleaned) // 2
+
+    left = cleaned[center - width]
+    right = cleaned[center + width]
+
+    replacement = (left + right) / 2
+
+    cleaned[center - width : center + width] = replacement
+
+    return cleaned
+
+# lo (minimum frequency) to hi (maximum frequency) in MHz.
+def readRange(lo, hi, bw):  
+    for center_f in np.arange(lo, hi, bw):
+        sdr.center_freq = center_f * 1e6
+        samples = sdr.read_samples(256*1024)
+
+        Pxx, freqs = plt.psd(
+            samples,
+            NFFT = 1024,
+            Fs = bw,
+            Fc = center_f,
+            visible = False
+        )
+        
+        Pxx_clean = remove_block_dc_spike(Pxx, freqs)
+
+        block_freqs.append(freqs)
+        block_psd_charts.append(10 * np.log10(Pxx_clean))   
+
+BANDWIDTH_MHZ = 2.4
+STEP_MHZ = 2.4
+
+configRTL(BANDWIDTH_MHZ, 36)
+
+time_start = strftime("%d.%m.%Y %H:%M:%S", gmtime());
+
+range_lo = 5
+range_hi = 800
+range_span = (range_hi - range_lo)
+range_mid = range_lo + (range_span / 2)
+
+print(range_span)
+print(range_mid)
+
+samples = readRange(range_lo, range_hi, STEP_MHZ);
+
+sdr.close()
+
+for f, p in zip(block_freqs, block_psd_charts):
+    plt.plot(f, p, color="red")
+
+time_end = strftime("%H:%M:%S", gmtime());
+
+plt.title("Spectrum " + time_start + " - " + time_end)
+plt.xlabel('Frequency (MHz)')
+plt.ylabel('Power (dB)')
+
+plt.show()
